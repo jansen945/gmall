b2387910a68954273c01b86d5aa9bf4b952012cb
sheng1.zhang <824890106@qq.com>
2022-08-30 19:53:21 +0800

    网关搭建

--- gateway/src/main/java/com/atguigu/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/atguigu/gateway/GatewayApplication.java
@@ -1,6 +1,5 @@
-package com.java.gmall.demo;
+package com.atguigu.gateway;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -8,12 +7,10 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableSwagger2
-@MapperScan("com.java.gmall.demo.dao")
-public class GmallPmsApplication {
+public class GatewayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GmallPmsApplication.class, args);
+        SpringApplication.run(GatewayApplication.class, args);
     }
 
 }
